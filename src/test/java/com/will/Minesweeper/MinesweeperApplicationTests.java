f543c0faa173dfdef2608c4885c324d641de9451
WillNeiman <willneimann@gmail.com>
2023-05-07 15:31:30 +0900

    Initial commit

---
+++ src/test/java/com/will/Minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.Minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
